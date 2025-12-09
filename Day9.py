c06652d3543bdce16930215d7a231f97006d3be4
Charlie Pilkington <pilkington216@gmail.com>
2025-12-09 18:15:25 +0000

    Day9 Part1 complete

---
+++ Day9.py
@@ -0,0 +1,90 @@
+import os
+
+def GetInput():
+    with open(os.getcwd() + "/test.txt") as file:
+        points = [tuple(map(int, line.split(','))) for line in file.readlines()]
+    return points
+
+def Lines(points):
+    vertical = list()
+    horizontal = list()
+    n = len(points)
+    index = 0
+    current = points[0]
+    while index < n:
+        i = (index + 1) % n
+
+        if current[1] > points[i][1]:
+            vertical.append((current[0] , (points[i][1], current[1])))
+            
+        elif current[1] < points[i][1]:
+            vertical.append((current[0] , (current[1], points[i][1])))
+            
+        elif current[0] > points[i][0]:
+            horizontal.append(((points[i][0], current[0]) , current[1]))
+            
+        elif current[0] < points[i][0]:
+            horizontal.append(((current[0], points[i][0]) , current[1]))
+
+        current = points[i]
+
+        index += 1
+
+    return vertical, horizontal
+
+def Intersect(a, b, vertical, horizontal):
+    x0 = min(a[0], b[0])
+    x1 = max(a[0], b[0])
+
+    y0 = min(a[1], b[1])
+    y1 = max(a[1], b[1])
+    
+    for line in horizontal:
+        if not(line[0][1] < x0 or line[0][0] > x1) and (y0 < line[1] < y1):
+            return 0
+
+    for line in vertical:
+        if not(line[1][1] < y0 or line[1][0] > y1) and (x0 < line[0] < x1):
+            return 0
+
+    return Area(a, b)
+
+def Area(a, b):
+    return abs((a[0] - b[0] + 1)*(a[1] - b[1] + 1))
+
+def Part1(points):
+    n = len(points)
+    highest_area = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            area = Area(points[i], points[j])
+            if area > highest_area:
+                highest_area = area
+    return highest_area
+
+def Part2(points):
+    vertical, horizontal = Lines(points)
+    n = len(points)
+    highest_area = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            area = Intersect(points[i], points[j], vertical, horizontal)
+
+            print(points[i], points[j])
+            print(area)
+            print()
+            if area > highest_area:
+                highest_area = area
+    return highest_area
+
+points = GetInput()
+
+ans = Part1(points)
+print(ans)
+
+ans = Part2(points)
+print(ans)
+
+
